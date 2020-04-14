5ce363e92623c859a7bf2634ced1581272c7c7e6
Saumil-Jariwala <jarivalasaumil@gmail.com>
2020-04-14 19:00:22 +0530

    Create new Angular Project, Added Material, Material > Autocomplete, Form Fields, Checkbox

---
+++ src/app/checkbox-overview/checkbox-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-checkbox-overview',
+  templateUrl: './checkbox-overview.component.html',
+  styleUrls: ['./checkbox-overview.component.css']
+})
+export class CheckboxOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
